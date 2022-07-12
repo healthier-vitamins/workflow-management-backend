ca9f446f9203938bb7facc292483a93be50f051f
healthier-vitamins <88876449+healthier-vitamins@users.noreply.github.com>
2022-07-13 04:32:31 +0800

    migration works

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    
+    SECRET_KEY = os.environ['SECRET_KEY'] or 'very-secretive-indeed-failed'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
