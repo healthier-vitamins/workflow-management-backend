d7f2d4fb231a1e203345689d0fb72ef28068ac07
healthier-vitamins <88876449+healthier-vitamins@users.noreply.github.com>
2022-07-13 19:35:39 +0800

    heroku postgres

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or "postgresql://office_user:PORKlingto1997@localhost/office_flow_management"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY') or 'very-secretive-indeed-failed'
