aba73e0c81d3734b5a1203ed98d29d8a4ce12623
healthier-vitamins <88876449+healthier-vitamins@users.noreply.github.com>
2022-07-13 20:00:18 +0800

    flask cors

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app_inner = Flask(__name__)
+CORS(app_inner)
 app_inner.config.from_object(Config)
 db = SQLAlchemy(app_inner)
 migrate = Migrate(app_inner, db)
